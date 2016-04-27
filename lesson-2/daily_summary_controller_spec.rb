e59a515d379f035687c7c69ea6413218760f828c
Vasily Polovnyov <vasily@polovnyov.ru>
2016-04-27 22:01:31 +0300

    add DailySummaryController example

---
+++ lesson-2/daily_summary_controller_spec.rb
@@ -0,0 +1,62 @@
+require "spec_helper"
+
+class DailySummaryController
+  attr_reader :current_user
+
+  def initialize(current_user:)
+    @current_user = current_user
+  end
+
+  def test
+    DailySummaryEmail.new(recipient: current_user).test
+
+    200
+  end
+
+  def deliver
+    subscribers.each do |user|
+      DailySummaryEmail.new(recipient: user).deliver
+    end
+
+    201
+  end
+
+  private
+
+  def subscribers
+    User.all.select(&:email_notifications_enabled?)
+  end
+end
+
+class DailySummaryEmail
+  attr_reader :recipient
+
+  def initialize(recipient:)
+    @recipient = recipient
+  end
+
+  def test
+    # собираем письмо для пользователя и отправляем его с пометкой [Test] в теме письма
+  end
+
+  def deliver
+    # собираем и отправляем письмо
+  end
+end
+
+class User
+  attr_reader :email, :email_notifications_enabled
+  alias :email_notifications_enabled? :email_notifications_enabled
+
+  def initialize(email:, email_notifications_enabled: true)
+    @email = email
+    @email_notifications_enabled = email_notifications_enabled
+  end
+
+  def self.all
+    # какая-то супер-сложная выборка пользователей из БД или внешнего API
+  end
+end
+
+RSpec.describe DailySummaryController do
+end
