efbdbf8a72b80510e206ad44b26fd0cbc32e5f68
Vasily Polovnyov <vasily@polovnyov.ru>
2016-05-21 18:48:20 +0300

    add BuildReportEmail spec

---
+++ lesson-4/build_report_email_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+RSpec.describe "BuildReportEmail" do
+  context "text method" do
+    it "should have proper subject" do
+    end
+
+    it "should include error links when there are error" do
+    end
+
+    it "should not include error links when there are no errors" do
+    end
+
+    it "should include success message when there are no errors" do
+    end
+  end
+
+  context "#deliver" do
+    it "calls mail#deliver_to() method" do
+    end
+  end
+end
