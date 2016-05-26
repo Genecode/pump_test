1867be41f5a23334236ed187a41adfd7679d7779
Vasily Polovnyov <vasily@polovnyov.ru>
2016-05-26 15:58:12 +0300

    add Account.active spec

---
+++ lesson-5/accounts_controller_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+REPOSITORY = {
+  account: [],
+  pending_account: []
+}.freeze
+
+class Account
+  def self.active
+    REPOSITORY[:account]
+  end
+end
+
+def create_list(what, amount)
+  model = double(what)
+  amount.times { REPOSITORY[what] << model }
+
+  REPOSITORY[what]
+end
+
+describe Account do
+  context "#active" do
+    before do
+      @active_accounts = create_list(:account, 2)
+      @pending_accounts = create_list(:pending_account, 3)
+    end
+
+    it "should return only active accounts" do
+      expect(Account.active).to match_array(@active_accounts)
+    end
+  end
+end
